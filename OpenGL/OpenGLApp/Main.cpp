3d89cddbddf374676cb5c03b2b23ecc819503d43
Utsuho <minusie357@gmail.com>
2024-02-21 01:04:05 +0900

    [Opengl] test uniform variable about move triangle left and right

--- OpenGL/OpenGLApp/Main.cpp
+++ OpenGL/OpenGLApp/Main.cpp
@@ -8,26 +8,33 @@ constexpr GLint HEIGHT = 600;
 GLuint VAO{};
 GLuint VBO{};
 GLuint shader{};
+GLuint uniformXMove{};
+
+bool direction = true;
+float triangleOffset = 0.f;
+float triangleMaxOffset = 0.7f;
+float triangleIncrement = 0.005f;
 
 // Vertex Shader
 static const char* vertexShader = 
-"#version 330\n\
-\n\
-layout (location = 0) in vec3 pos;	\n\
-\n\
-void main()	\n\
-{	\n\
-	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);	\n\
+"#version 330												\n\
+                                                            \n\
+layout (location = 0) in vec3 pos;							\n\
+                                                            \n\
+uniform float xMove;										\n\
+void main()													\n\
+{															\n\
+	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, 0.4 * pos.z, 1.0);	\n\
 }";
 
 static const char* fragmentShader = 
-"#version 330\n\
-\n\
-out vec4 colour;	\n\
-\n\
-void main()	\n\
-{	\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);	\n\
+"#version 330												\n\
+															\n\
+out vec4 colour;											\n\
+															\n\
+void main()													\n\
+{															\n\
+	colour = vec4(1.0, 0.0, 0.0, 1.0);						\n\
 }";
 
 void CreateTriangle()
@@ -86,7 +93,7 @@ void CompileShaders()
 		std::cout << "Error creating shader program" << std::endl;
 		return;
 	}
-	
+
 	AddShader(shader, vertexShader, GL_VERTEX_SHADER);
 	AddShader(shader, fragmentShader, GL_FRAGMENT_SHADER);
 
@@ -97,7 +104,7 @@ void CompileShaders()
 	if (result == 0)
 	{
 		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
-		std::cout << "error linking program : "<< eLog << "\n";
+		std::cout << "error linking program : " << eLog << "\n";
 		return;
 	}
 
@@ -109,6 +116,8 @@ void CompileShaders()
 		std::cout << "Error validating program : " << eLog << "\n";
 		return;
 	}
+
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 int main()
@@ -120,7 +129,7 @@ int main()
 		glfwTerminate();
 		return 1;
 	}
-	
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -138,7 +147,7 @@ int main()
 	int bufferHeight{};
 	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
 	glfwMakeContextCurrent(mainWindow);
-	
+
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
 	{
@@ -156,10 +165,25 @@ int main()
 	{
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triangleOffset += triangleIncrement;
+		}
+		else
+		{
+			triangleOffset -= triangleIncrement;
+		}
+
+		if (std::abs(triangleOffset) >= triangleMaxOffset)
+		{
+			direction = !direction;
+		}
+
 		glClearColor(0.f, 0.f, 0.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+		glUniform1f(uniformXMove, triangleOffset);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
