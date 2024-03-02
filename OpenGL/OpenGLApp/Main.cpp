2baae25a9bb0da3d99d53262bef3b96625bd0695
Utsuho <minusie357@gmail.com>
2024-03-02 18:05:30 +0900

    [Opengl] replace uniformXMove to modelMatrix

--- OpenGL/OpenGLApp/Main.cpp
+++ OpenGL/OpenGLApp/Main.cpp
@@ -1,6 +1,12 @@
 #include "precomp.h"
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 
 constexpr GLint WIDTH = 800;
 constexpr GLint HEIGHT = 600;
@@ -8,7 +14,7 @@ constexpr GLint HEIGHT = 600;
 GLuint VAO{};
 GLuint VBO{};
 GLuint shader{};
-GLuint uniformXMove{};
+GLuint uniformModel{};
 
 bool direction = true;
 float triangleOffset = 0.f;
@@ -21,10 +27,10 @@ static const char* vertexShader =
                                                             \n\
 layout (location = 0) in vec3 pos;							\n\
                                                             \n\
-uniform float xMove;										\n\
+uniform mat4 modelMatrix;									\n\
 void main()													\n\
 {															\n\
-	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, 0.4 * pos.z, 1.0);	\n\
+	gl_Position = modelMatrix * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);	\n\
 }";
 
 static const char* fragmentShader = 
@@ -67,7 +73,7 @@ void AddShader(GLuint program, const std::string& shaderCode, GLenum shaderType)
 	GLint codeLengths[1];
 	
 	codes[0] = shaderCode.c_str();
-	codeLengths[0] = shaderCode.length();
+	codeLengths[0] = static_cast<GLint>(shaderCode.length());
 
 	glShaderSource(theShader, 1, codes, codeLengths);
 	glCompileShader(theShader);
@@ -117,7 +123,7 @@ void CompileShaders()
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "modelMatrix");
 }
 
 int main()
@@ -179,11 +185,17 @@ int main()
 			direction = !direction;
 		}
 
+		// set color when screen(actually color buffer) is cleared
 		glClearColor(0.f, 0.f, 0.f, 1.f);
+		// parameter available : GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_STENCIL_BUFFER_BIT
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
-		glUniform1f(uniformXMove, triangleOffset);
+
+		glm::mat4 modelMatrix{ 1.0f };
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(triangleOffset, 0.f, 0.f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(modelMatrix));
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
